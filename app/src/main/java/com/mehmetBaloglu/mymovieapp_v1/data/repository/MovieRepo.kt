af66e931387bd705d838f6cbc087029ababa1cc6
muameh <balmehmet111@gmail.com>
2024-06-27 23:31:25 +0300

    ide üzeriden deneme 1

--- app/src/main/java/com/mehmetBaloglu/mymovieapp_v1/data/repository/MovieRepo.kt
+++ app/src/main/java/com/mehmetBaloglu/mymovieapp_v1/data/repository/MovieRepo.kt
@@ -8,8 +8,8 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.firestore
-import com.mehmetBaloglu.mymovieapp_v1.data.models.firebase_response.ForFirebaseResponse
 import com.mehmetBaloglu.mymovieapp_v1.data.models.detailseries.DetailSerieResponse
+import com.mehmetBaloglu.mymovieapp_v1.data.models.firebase_response.ForFirebaseResponse
 import com.mehmetBaloglu.mymovieapp_v1.data.models.general_returns.FilmItem
 import com.mehmetBaloglu.mymovieapp_v1.data.models.general_returns.GeneralResponse
 import com.mehmetBaloglu.mymovieapp_v1.data.models.general_returns.detail.DetailFilmResponse
@@ -22,7 +22,7 @@ import kotlinx.coroutines.withContext
 class MovieRepo(var apiDao: ApiDao) {
 
     private var auth: FirebaseAuth
-    private var db :FirebaseFirestore
+    private var db: FirebaseFirestore
 
     init {
         auth = Firebase.auth
@@ -30,38 +30,38 @@ class MovieRepo(var apiDao: ApiDao) {
     }
 
     //--------------------- MOVIES -------------------------------------//
-    suspend fun getPopularMovies(page: Int) : GeneralResponse =
-        withContext(Dispatchers.IO) { apiDao.getPopularMovies(page= page) }
+    suspend fun getPopularMovies(page: Int): GeneralResponse =
+        withContext(Dispatchers.IO) { apiDao.getPopularMovies(page = page) }
 
-    suspend fun getMoviesInTheaters() : List<FilmItem> =  //vizyondakiler
+    suspend fun getMoviesInTheaters(): List<FilmItem> =  //vizyondakiler
         withContext(Dispatchers.IO) { apiDao.getMoviesInTheaters().filmItems }
 
-    suspend fun getUpcomingMovies() : List<FilmItem> =
-        withContext(Dispatchers.IO) { apiDao.getUpcomingMovies().filmItems}
+    suspend fun getUpcomingMovies(): List<FilmItem> =
+        withContext(Dispatchers.IO) { apiDao.getUpcomingMovies().filmItems }
 
-    suspend fun getTopRatedMovies(page: Int) : GeneralResponse =
-        withContext(Dispatchers.IO) { apiDao.getTopRatedMovies(page= page)}
+    suspend fun getTopRatedMovies(page: Int): GeneralResponse =
+        withContext(Dispatchers.IO) { apiDao.getTopRatedMovies(page = page) }
 
     //--------------------- TV SERIES -------------------------------------//
-    suspend fun getTopRatedTVSeries(page: Int) : SeriesResponse =
-        withContext(Dispatchers.IO) { apiDao.getTopRatedSeries(page= page)}
+    suspend fun getTopRatedTVSeries(page: Int): SeriesResponse =
+        withContext(Dispatchers.IO) { apiDao.getTopRatedSeries(page = page) }
 
     suspend fun getPopularTVSeries(page: Int): SeriesResponse =
-        withContext(Dispatchers.IO) { apiDao.getPopularTVSeries(page= page)}
+        withContext(Dispatchers.IO) { apiDao.getPopularTVSeries(page = page) }
 
     //--------------------- SEARCH -------------------------------------//
-    suspend fun searchMovie(query: String) : List<FilmItem> =
-        withContext(Dispatchers.IO) {apiDao.searchMovies(query).filmItems }
+    suspend fun searchMovie(query: String): List<FilmItem> =
+        withContext(Dispatchers.IO) { apiDao.searchMovies(query).filmItems }
 
-    suspend fun searchSeries(query: String) : List<SeriesItem> =
-        withContext(Dispatchers.IO) {apiDao.searchSeries(query).seriesItems}
+    suspend fun searchSeries(query: String): List<SeriesItem> =
+        withContext(Dispatchers.IO) { apiDao.searchSeries(query).seriesItems }
 
     //--------------------- DETAILS ---------------------------------------//
-    suspend fun getMovieDetails(id : Int) : DetailFilmResponse =
-        withContext(Dispatchers.IO) {apiDao.getMovieDetails(id)}
+    suspend fun getMovieDetails(id: Int): DetailFilmResponse =
+        withContext(Dispatchers.IO) { apiDao.getMovieDetails(id) }
 
-    suspend fun getSerieDetails(id : Int) : DetailSerieResponse =
-        withContext(Dispatchers.IO) { apiDao.getSerieDetails(id)}
+    suspend fun getSerieDetails(id: Int): DetailSerieResponse =
+        withContext(Dispatchers.IO) { apiDao.getSerieDetails(id) }
 
     //------------------------DISCOVER----------------------------------------------//
 
@@ -73,23 +73,21 @@ class MovieRepo(var apiDao: ApiDao) {
         voteAverageLte: Double,
         runtimeGte: Int,
         runtimeLte: Int,
-        voteCount : Int,
-        keyword : String?
-    ): List<FilmItem> =
-        withContext(Dispatchers.IO) {
-            apiDao.discoverMovies(
-                releaseDateGte = releaseDateGte,
-                releaseDateLte = releaseDateLte,
-                withGenres = withGenres,
-                voteAverageGte = voteAverageGte,
-                voteAverageLte = voteAverageLte,
-                runtimeGte = runtimeGte,
-                runtimeLte = runtimeLte,
-                voteCountGte = voteCount,
-                withKeywords = keyword
-            )
-                .filmItems
-        }
+        voteCount: Int,
+        keyword: String?
+    ): List<FilmItem> = withContext(Dispatchers.IO) {
+        apiDao.discoverMovies(
+            releaseDateGte = releaseDateGte,
+            releaseDateLte = releaseDateLte,
+            withGenres = withGenres,
+            voteAverageGte = voteAverageGte,
+            voteAverageLte = voteAverageLte,
+            runtimeGte = runtimeGte,
+            runtimeLte = runtimeLte,
+            voteCountGte = voteCount,
+            withKeywords = keyword
+        ).filmItems
+    }
 
     //--------------------------------Firebase-------------------------------//
 
@@ -97,8 +95,7 @@ class MovieRepo(var apiDao: ApiDao) {
     fun createUserWatchList(): MutableLiveData<List<ForFirebaseResponse>> {
 
         var usersEmailAdress = auth.currentUser?.email.toString()
-        db.collection("WatchList")
-            .whereEqualTo("email", usersEmailAdress)
+        db.collection("WatchList").whereEqualTo("email", usersEmailAdress)
             .addSnapshotListener { value, error ->
                 if (value != null) {
                     val _tempWatchList = ArrayList<ForFirebaseResponse>()
@@ -122,8 +119,7 @@ class MovieRepo(var apiDao: ApiDao) {
     fun createUserWatchEDList(): MutableLiveData<List<ForFirebaseResponse>> {
         var usersEmailAdress = auth.currentUser?.email.toString()
 
-        db.collection("WatchedList")
-            .whereEqualTo("email", usersEmailAdress)
+        db.collection("WatchedList").whereEqualTo("email", usersEmailAdress)
             .addSnapshotListener { value, error ->
                 if (value != null) {
                     val _tempWatchedList = ArrayList<ForFirebaseResponse>()
@@ -143,11 +139,9 @@ class MovieRepo(var apiDao: ApiDao) {
     }
 
     //-----------------------------------------------------------------
-    fun deleteFromWatchEDList(context: Context,filmID: String) {
+    fun deleteFromWatchEDList(context: Context, filmID: String) {
 
-        db.collection("WatchedList")
-            .whereEqualTo("filmID", filmID)
-            .get()
+        db.collection("WatchedList").whereEqualTo("filmID", filmID).get()
             .addOnSuccessListener { querySnapshot ->
                 if (!querySnapshot.isEmpty) {
                     val document = querySnapshot.documents[0]
@@ -155,7 +149,8 @@ class MovieRepo(var apiDao: ApiDao) {
                     db.collection("WatchedList").document(document.id).delete()
                         .addOnSuccessListener {
                             println("Document successfully deleted!")
-                            Toast.makeText(context,"deleted from WatchedList",Toast.LENGTH_SHORT).show()
+                            Toast.makeText(context, "deleted from WatchedList", Toast.LENGTH_SHORT)
+                                .show()
                         }.addOnFailureListener { e -> println("Error deleting document: $e") }
                 } else {
                     println("No documents found with the given ID")
@@ -164,40 +159,24 @@ class MovieRepo(var apiDao: ApiDao) {
     }
     //-----------------------------------------------------------
 
-    fun deleteFromWatchList(context: Context,filmID: String) {
+    fun deleteFromWatchList(context: Context, filmID: String) {
 
-        db.collection("WatchList")
-            .whereEqualTo("filmID", filmID)
-            .get()
+        db.collection("WatchList").whereEqualTo("filmID", filmID).get()
             .addOnSuccessListener { querySnapshot ->
                 if (!querySnapshot.isEmpty) {
                     val document = querySnapshot.documents[0]
 
-                    db.collection("WatchList").document(document.id).delete()
-                        .addOnSuccessListener {
-                            println("Document successfully deleted!")
-                            Toast.makeText(context,"deleted from WatchList",Toast.LENGTH_SHORT).show()
-                        }
-                        .addOnFailureListener { e ->
-                            println("Error deleting document: $e")
-                        }
+                    db.collection("WatchList").document(document.id).delete().addOnSuccessListener {
+                        println("Document successfully deleted!")
+                        Toast.makeText(context, "deleted from WatchList", Toast.LENGTH_SHORT).show()
+                    }.addOnFailureListener { e ->
+                        println("Error deleting document: $e")
+                    }
                 } else {
                     println("No documents found with the given ID")
                 }
             }
     }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 }
